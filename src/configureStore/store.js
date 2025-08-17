7c7ff8a740e26c956f0b3e3609787aeb9f73166a
Sofia Slobodskaya <soniakorolyok@gmail.com>
2025-08-17 18:45:26 +0300

    add rtk

--- src/configureStore/store.js
+++
@@ -1,6 +0,0 @@
-import {applyMiddleware, legacy_createStore as createStore} from "redux";
-import {weatherReducer} from "../reducer/weatherReducer.js";
-import {thunk} from "redux-thunk";
-
-
-export const store = createStore(weatherReducer, applyMiddleware(thunk));
--- src/features/api/weatherActions.js
+++ src/features/api/weatherActions.js
@@ -1,32 +1,20 @@
-import {api_key, base_url} from "../utils/constans.js";
-
-export const SET_WEATHER = "SET_WEATHER";
-export const SET_MESSAGE = "SET_MESSAGE";
-
-
-export const satWeather = (weather) => ({
-    type: SET_WEATHER,
-    payload: weather
-});
-
-export const setMessage = (message) => ({
-    type: SET_MESSAGE,
-    payload: message
-})
+import {setMessage} from "../message/messageSlice.js";
+import {api_key, base_url} from "../../utils/constans.js";
+import {setWeather} from "../weather/weatherSlice.js";
 
 export const fetchWeather = (city) => {
-    return dispatch => {
+    return (dispatch) => {
         fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`)
             .then(res => res.json())
             .then(data => {
-                dispatch(satWeather({
+                dispatch(setWeather({
                     country: data.sys.country,
                     city: data.name,
                     temp: data.main.temp,
                     pressure: data.main.pressure,
                     sunset: (new Date(data.sys.sunset * 1000)).toLocaleTimeString()
                 }));
-                // dispatch(setMessage(""))
+                dispatch(setMessage(""))
             })
             .catch(err => {
                 console.log(err);
